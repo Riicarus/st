fc58b1d757fd4f8d35de62126d40ace8c91ab5dd
Riicarus <riicarus.acc@gmail.com>
2024-02-11 18:42:19 +0800

    patch&config: screen scroll

--- config.h
+++ config.h
@@ -214,6 +214,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ MODKEY,     	       	XK_bracketleft,    	kscrollup,      {.i =  1} },
+	{ MODKEY,           	XK_bracketright,  	kscrolldown,    {.i =  1} },
+	{ MODKEY|ControlMask,	XK_bracketleft,		kscrollup, 	{.i = -1} },
+	{ MODKEY|ControlMask,	XK_bracketright,	kscrolldown,	{.i = -1} },
 };
 
 /*
